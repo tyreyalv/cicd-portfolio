dfb2818a207ef386712d4bcb68565329d52d2340
tyreyalv <accounts@tyreyalv.com>
2025-07-18 22:41:29 -0600

    fix: Update welcome message to enhance user engagement

--- app/app.py
+++ app/app.py
@@ -59,8 +59,9 @@ def hello():
     </head>
     <body>
         <div class="container">
-            <h1>Welcome to the Demo App Team and Group</h1>
+            <h1>Welcome to the Demo App Beautiful Lady</h1>
             <p>This application is deployed via a CI/CD pipeline.</p>
+            <p>Kicking ass, taking names.</p>
             <div class="version-badge">Version: {APP_VERSION}</div>
         </div>
     </body>
